aa3d047f0cf5d3f8258532b13551fcf652ce75c4
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-08 10:53:45 +0900

    1つ開けたら他のアコーディオンが閉じる

--- accordion/js/common.js
+++ accordion/js/common.js
@@ -3,9 +3,19 @@
 //dt取得してaccordionHeaderとする
 const accordionHeader = document.querySelectorAll('dt');
 
-//accordionごとに繰り返す
+//accordion全てで実行する
 accordionHeader.forEach((accordion) => {
   accordion.addEventListener('click', () => {
-    accordionHeader.parentNode.classList.toggle('open');
+    //クリックされた親要素にopenをつける
+    const parentItem = accordion.parentNode;
+    parentItem.classList.toggle('open');
+
+    //クリックされた親要素以外を閉じる
+    accordionHeader.forEach((otherAccordion) => {
+      if (otherAccordion !== accordion) {
+        const otherParentItem = otherAccordion.parentNode;
+        otherParentItem.classList.remove('open');
+      }
+    });
   });
 });
